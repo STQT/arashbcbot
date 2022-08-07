fa47dd34bc494039eebc1477881f87396aef99c5
Kairat Sultan <garri.sultonov@mail.ru>
2022-08-07 09:42:03 +0500

    back to telebot

--- configs.py
+++ configs.py
@@ -21,12 +21,3 @@ if not DEBUG:
         datefmt='%Y-%m-%d %H:%M:%S',
         level=logging.WARNING
     )
-
-# webhook settings
-WEBHOOK_HOST = os.getenv('HEROKU_APP_URL')
-WEBHOOK_PATH = '/'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'  # or ip
-WEBAPP_PORT = os.getenv('PORT', default=8000)
